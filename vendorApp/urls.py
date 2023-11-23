7d8710901eb94408a55f8e37ec3976c8f7cf8db9
amisalaam <amisalaam9@gmail>
2023-11-23 17:29:39 +0530

    vendor management completed

--- vendorApp/urls.py
+++ vendorApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import VendorCreateView
+from .views import VendorCreateView,VenodrDetailsView
 
 urlpatterns = [
-    path('api/vendors/', VendorCreateView.as_view(), name='vendor-create')
-    
+    path('api/vendors/', VendorCreateView.as_view(), name='vendor-create'),
+    path('api/vendors/<int:vendor_id>/', VenodrDetailsView.as_view(), name='vendor-detail'),
 ]
--- vendorApp/views.py
+++ vendorApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import VendorSerialzers,AllVendorSerializers
+from .serializers import VendorSerialzers, AllVendorSerializers
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Vendor
@@ -9,7 +9,7 @@ from .models import Vendor
 # Create your views here.
 
 class VendorCreateView(APIView):
-    def post(self,request):
+    def post(self, request):
         serializer = VendorSerialzers(data=request.data)
 
         if serializer.is_valid():
@@ -18,14 +18,50 @@ class VendorCreateView(APIView):
                 **serializer.data,
                 'vendor_code': vendor.vendor_code,
             }
-            
             return Response(response_data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
 
-    def get(self,request):
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
         vendors = Vendor.objects.all()
-        serializer = AllVendorSerializers(vendors,many=True)
+        serializer = AllVendorSerializers(vendors, many=True)
 
         return Response(serializer.data)
+
+
+class VenodrDetailsView(APIView):
+    def get(self, request, vendor_id):
+        try:
+            vendor = Vendor.objects.get(pk=vendor_id)
+        except Vendor.DoesNotExist:
+            return Response({"error": "Vendor not found "}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = AllVendorSerializers(vendor)
+
+        return Response(serializer.data)
+
+
+    def put(self, request, vendor_id):
+        try:
+            vendor = Vendor.objects.get(pk=vendor_id)
+        except Vendor.DoesNotExist:
+            return Response({"error": "Vendor not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = VendorSerialzers(vendor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def delete(self, request, vendor_id):
+        try:
+            vendor = Vendor.objects.get(pk=vendor_id)
+        except Vendor.DoesNotExist:
+            return Response({"error": "Vendor not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        vendor.delete()
+        return Response({"success":"Vendor deleted successfully"},status=status.HTTP_204_NO_CONTENT)
+    
+
+
